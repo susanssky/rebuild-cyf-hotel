4814bd8a0a61a4151bc2ac8137409b14142fb804
susanssky <107769663+susanssky@users.noreply.github.com>
2023-11-30 17:25:48 +0000

    minor updating

--- server/server.ts
+++ server/server.ts
@@ -15,18 +15,18 @@ app.use(cors())
 export const client = new Client(ClientConfig)
 client.connect()
 
-const seedQuery = fs.readFileSync(
-  path.resolve(__dirname, './utils/seeding.sql'),
-  {
-    encoding: 'utf8',
-  }
-)
+// const seedQuery = fs.readFileSync(
+//   path.resolve(__dirname, '../database/seeding.sql'),
+//   {
+//     encoding: 'utf8',
+//   }
+// )
 
-client.query(seedQuery, (err, res) => {
-  console.log(err, res)
-  console.log('Seeding Completed!')
-  // client.end()
-})
+// client.query(seedQuery, (err, res) => {
+//   console.log(err, res)
+//   console.log('Seeding Completed!')
+//   // client.end()
+// })
 app.use('/', customerRoutes)
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server is listening on post ${process.env.SERVER_PORT}`)
