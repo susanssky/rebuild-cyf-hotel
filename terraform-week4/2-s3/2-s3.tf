aa7c4ebfe0fd341443f6a566a7d7aba4ce061cae
susanssky <107769663+susanssky@users.noreply.github.com>
2023-12-10 13:59:44 +0000

    a bit update

--- terraform-week4/2-s3/2-s3.tf
+++ terraform-week4/2-s3/2-s3.tf
@@ -2,21 +2,9 @@
 module "frontend" {
   source = "./s3_module"
 }
-# This resource will destroy (potentially immediately) after null_resource.next
-resource "null_resource" "previous" {}
 
-resource "time_sleep" "wait_30_seconds" {
-  depends_on = [null_resource.previous]
-
-  create_duration = "30s"
-}
-
-# This resource will create (at least) 30 seconds after null_resource.previous
-resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_30_seconds]
-}
 output "frontend-url" {
-  value = module.frontend.cyf-week4-frontend.website_endpoint
+  value = module.frontend.url
 }
 # ======================= moved to s3_module =====================
 # resource "aws_s3_bucket" "frontend" {
--- terraform-week4/2-s3/s3_module/3-outputs.tf
+++ terraform-week4/2-s3/s3_module/3-outputs.tf
@@ -1,5 +1,3 @@
-# for output the bucket all attribute, 
-# can think "const cyf-week4-frontend = aws_s3_bucket.frontend"
-output "cyf-week4-frontend" {
-  value = aws_s3_bucket.frontend
+output "url" {
+  value = aws_s3_bucket_website_configuration.frontend-website.website_endpoint
 }
