e4cf1daf9bf4c276111a11bf45f76f9b69a09f64
susanssky <107769663+susanssky@users.noreply.github.com>
2023-12-10 13:34:05 +0000

    a bit update

--- terraform-week4/2-s3/2-s3.tf
+++ terraform-week4/2-s3/2-s3.tf
@@ -1,23 +1,23 @@
 # call s3 module
-# module "frontend" {
-#   source = "./s3_module"
-# }
-# # This resource will destroy (potentially immediately) after null_resource.next
-# resource "null_resource" "previous" {}
+module "frontend" {
+  source = "./s3_module"
+}
+# This resource will destroy (potentially immediately) after null_resource.next
+resource "null_resource" "previous" {}
 
-# resource "time_sleep" "wait_30_seconds" {
-#   depends_on = [null_resource.previous]
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.previous]
 
-#   create_duration = "30s"
-# }
+  create_duration = "30s"
+}
 
-# # This resource will create (at least) 30 seconds after null_resource.previous
-# resource "null_resource" "next" {
-#   depends_on = [time_sleep.wait_30_seconds]
-# }
-# output "frontend-url" {
-#   value = module.frontend.cyf-week4-frontend.website_endpoint
-# }
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
+}
+output "frontend-url" {
+  value = module.frontend.cyf-week4-frontend.website_endpoint
+}
 # ======================= moved to s3_module =====================
 # resource "aws_s3_bucket" "frontend" {
 #   bucket = "cyf-cloud-week4"
