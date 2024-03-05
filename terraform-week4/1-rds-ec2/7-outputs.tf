86dc285d6a7e6173795cbc2de3a5016176539364
susanssky <107769663+susanssky@users.noreply.github.com>
2024-03-05 20:39:54 +0000

    week4 updated

--- terraform-week4/1-rds-ec2/7-outputs.tf
+++ terraform-week4/1-rds-ec2/7-outputs.tf
@@ -10,7 +10,7 @@ output "ec2_id" { value = aws_instance.backend.id }
 output "ec2_security_group_id" { value = aws_security_group.ec2-sg.id }
 
 output "key_name" { value = aws_key_pair.ssh-key.key_name }
-output "private_key" {
+output "ec2_private_key" {
   value     = tls_private_key.create-key-pair-for-ec2.private_key_pem
   sensitive = true
 }
