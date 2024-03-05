59aaf92aadaf4aea8c577b62dc3510b2238debbe
susanssky <107769663+susanssky@users.noreply.github.com>
2024-03-05 23:40:23 +0000

    week4 updated

--- terraform-week4/1-rds-ec2/6-ec2.tf
+++ terraform-week4/1-rds-ec2/6-ec2.tf
@@ -19,6 +19,7 @@ resource "aws_instance" "backend" {
   user_data                   = <<EOF
 #!/bin/bash
 sudo apt update
+sudo apt install postgresql -y
 sudo apt install stress -y
 sudo apt install docker.io -y
 sudo systemctl start docker
