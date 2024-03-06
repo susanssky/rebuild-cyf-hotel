63d1f5e4d0b6ed9ae94853fbe4b5a6ff0d8053e4
susanssky <107769663+susanssky@users.noreply.github.com>
2024-03-06 16:09:03 +0000

    week5 updated

--- terraform-week4/1-rds-ec2/7-outputs.tf
+++ terraform-week4/1-rds-ec2/7-outputs.tf
@@ -1,6 +1,6 @@
 output "vpc_id" { value = aws_vpc.vpc.id }
 output "public_subnet_id" { value = aws_subnet.public-subnet[*].id }
-output "private_subnet_id" { value = aws_subnet.private-subnet[*].id }
+# output "private_subnet_id" { value = aws_subnet.private-subnet[*].id }
 
 output "rds_endpoint" { value = aws_db_instance.database.endpoint }
 output "rds_security_group_id" { value = aws_security_group.rds-sg.id }
@@ -10,11 +10,11 @@ output "ec2_id" { value = aws_instance.backend.id }
 output "ec2_security_group_id" { value = aws_security_group.ec2-sg.id }
 
 output "key_name" { value = aws_key_pair.ssh-key.key_name }
-output "ec2_private_key" {
-  value     = tls_private_key.create-key-pair-for-ec2.private_key_pem
-  sensitive = true
-}
-output "ec2_public_key" {
-  value     = tls_private_key.create-key-pair-for-ec2.public_key_openssh
-  sensitive = true
-}
+# output "ec2_private_key" {
+#   value     = tls_private_key.create-key-pair-for-ec2.private_key_pem
+#   sensitive = true
+# }
+# output "ec2_public_key" {
+#   value     = tls_private_key.create-key-pair-for-ec2.public_key_openssh
+#   sensitive = true
+# }
