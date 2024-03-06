93e35c3041df400d288d712c437434c593a73e20
susanssky <107769663+susanssky@users.noreply.github.com>
2024-03-06 16:13:56 +0000

    week5 updated

--- terraform-week4/1-rds-ec2/7-outputs.tf
+++ terraform-week4/1-rds-ec2/7-outputs.tf
@@ -2,7 +2,7 @@ output "vpc_id" { value = aws_vpc.vpc.id }
 output "public_subnet_id" { value = aws_subnet.public-subnet[*].id }
 # output "private_subnet_id" { value = aws_subnet.private-subnet[*].id }
 
-output "rds_endpoint" { value = aws_db_instance.database.endpoint }
+# output "rds_endpoint" { value = aws_db_instance.database.endpoint }
 output "rds_security_group_id" { value = aws_security_group.rds-sg.id }
 
 output "ec2_public_ip" { value = aws_instance.backend.public_ip }
--- terraform-week5/1-rds-ec2/7-outputs.tf
+++ terraform-week5/1-rds-ec2/7-outputs.tf
@@ -3,7 +3,7 @@ output "public_subnet_id" { value = aws_subnet.public-subnet[*].id }
 # output "private_subnet_id" { value = aws_subnet.private-subnet[*].id }
 
 output "rds_identifier" { value = aws_db_instance.database.identifier } # for cloudwatch dashboard
-output "rds_endpoint" { value = aws_db_instance.database.endpoint }
+# output "rds_endpoint" { value = aws_db_instance.database.endpoint }
 output "rds_security_group_id" { value = aws_security_group.rds-sg.id }
 
 output "ec2_public_ip" { value = aws_instance.backend.public_ip }
